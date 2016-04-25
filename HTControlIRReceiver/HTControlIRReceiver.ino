79e572a0c1138ad37ed38c03d7017d719106b5ac
Jari Nevala <jari.nevala@optofidelity.com>
2016-04-25 15:15:21 +0300

    initial commit

---
+++ HTControlIRReceiver/HTControlIRReceiver.ino
@@ -0,0 +1,137 @@
+#include <IRremoteInt.h>
+#include <IRremote.h>
+#include <SoftwareSerial.h>
+
+//#define DEBUG true
+#define IR_RECEIVE_PIN 12
+#define WAIT_NEXT_COMMAND_LED_PIN 7
+#define soundBarPowerCommand 0x100C //Power key on soundbar remote
+#define HTPowerOnCommand 0x10DC //"Night mode on" on soundbar remote
+#define HTPowerOffCommand 0x10DE //"Auto volume on" on soundbar remote
+#define waitForSecondCommandTimeout 5 //time to wait for second command in seconds
+#define RS485_RECEIVE_PIN 10
+#define RS485_TRANSMIT_PIN 11
+
+IRrecv irrecv(IR_RECEIVE_PIN);
+IRsend irsend;
+decode_results results;
+
+SoftwareSerial RS485Serial(RS485_RECEIVE_PIN, RS485_TRANSMIT_PIN); // RX, TX
+
+bool waitingForSecondCommand = false; 
+int timer = 0; //timeout timer for the second command
+bool DEBUGPRINT = true;
+
+void setup()
+{
+	//if (DEBUGPRINT) 
+	Serial.begin(9600);
+	
+	irrecv.enableIRIn(); // Start the receiver
+	/*irrecv.blink13(true);*/	
+	pinMode(WAIT_NEXT_COMMAND_LED_PIN, OUTPUT);
+	digitalWrite(WAIT_NEXT_COMMAND_LED_PIN, LOW);
+	
+	debugPrint("HTControl IR receiver started");
+	
+	RS485Serial.begin(9600);   // set the data rate 
+	digitalWrite(3, HIGH);  // Init Transceiver   
+}
+
+void loop() {
+	if (irrecv.decode(&results)) {
+		//printReceivedIR(results);
+		
+		results.value &= 0xfeffff; //mask out title bit as per https://learn.adafruit.com/using-an-infrared-library/hardware-needed
+
+		if(!waitingForSecondCommand)
+		{		
+			if (results.decode_type == RC6 && results.value == soundBarPowerCommand)
+			{			
+				debugPrint("Power off at soundbar remote pressed");
+				waitingForSecondCommand = true;
+				digitalWrite(WAIT_NEXT_COMMAND_LED_PIN, HIGH);
+				timer = 0;
+				//delay(100);			
+			}
+		}
+		else
+		{
+			if (results.decode_type == RC6 && results.value == HTPowerOnCommand)
+			{				
+				debugPrint("HT on at soundbar remote pressed");
+				waitingForSecondCommand = false;
+				digitalWrite(WAIT_NEXT_COMMAND_LED_PIN, LOW);
+				RS485send("HTON");
+				//delay(100);
+			}
+			else if (results.decode_type == RC6 && results.value == HTPowerOffCommand)
+			{
+				debugPrint("HT off at soundbar remote pressed");
+				waitingForSecondCommand = false;
+				digitalWrite(WAIT_NEXT_COMMAND_LED_PIN, LOW);
+				RS485send("HTOFF");				
+				//delay(100);
+			}
+		}
+
+		irrecv.resume(); // Receive the next value
+	}	
+
+	if (waitingForSecondCommand)
+	{
+		delay(10);
+		timer += 10;
+		if (timer > waitForSecondCommandTimeout * 1000)
+		{
+			debugPrint("Timeout when waiting for second command");
+			waitingForSecondCommand = false;
+			digitalWrite(WAIT_NEXT_COMMAND_LED_PIN, LOW);
+		}
+	}
+}
+
+void RS485send(String dataString)
+{
+	RS485Serial.print(dataString);
+	debugPrint(dataString + " sent");
+}
+
+void debugPrint(String text)
+{
+	//if (DEBUGPRINT) 
+	Serial.println(text);
+}
+
+//print received IR command for debugging
+void printReceivedIR(decode_results results) {
+	Serial.print("--- Received: ---");
+	if (results.decode_type == NEC) {
+		Serial.print("NEC: ");
+	}
+	else if (results.decode_type == SONY) {
+		Serial.print("SONY: ");
+	}
+	else if (results.decode_type == RC5) {
+		Serial.print("RC5: ");
+	}
+	else if (results.decode_type == RC6) {
+		Serial.print("RC6: ");
+	}
+	else if (results.decode_type == UNKNOWN) {
+		Serial.print("UNKNOWN: ");
+	}
+
+	Serial.print("value: ");
+	Serial.println(results.value, HEX);
+	Serial.print("address: ");
+	Serial.println(results.address, HEX);
+	Serial.print("bits: ");
+	Serial.println(results.bits);
+	Serial.print("rawlen: ");
+	Serial.println(results.rawlen, HEX);
+	Serial.print("overflow: ");
+	Serial.println(results.overflow, HEX);
+	Serial.print("--------------");
+}
+
---
+++ HTControlMainUnit/HTControlMainUnit.ino
@@ -0,0 +1,60 @@
+#include <SoftwareSerial.h>
+
+#define RS485_IR_RECEIVER_RECEIVE_PIN 11
+#define RS485_IR_RECEIVER_TRANSMIT_PIN 12
+
+//#define RS485_PROJECTOR_RECEIVE_PIN ?
+//#define RS485_PROJECTOR_TRANSMIT_PIN ?
+
+#define RS232_HDMI_SWITCH_RECEIVE_PIN 5                              
+#define RS232_HDMI_SWITCH_TRANSMIT_PIN 6
+
+SoftwareSerial RS485_ir_receiver_serial(RS485_IR_RECEIVER_RECEIVE_PIN, RS485_IR_RECEIVER_TRANSMIT_PIN); // RX, TX
+SoftwareSerial RS232_HDMI_switch_serial(RS232_HDMI_SWITCH_RECEIVE_PIN, RS232_HDMI_SWITCH_TRANSMIT_PIN); // RX, TX
+
+String cmd;
+
+void setup()
+{
+	Serial.begin(9600);
+	while (!Serial) {
+		; // wait for serial port to connect. Needed for native USB port only
+	}
+
+	RS485_ir_receiver_serial.begin(9600);
+	RS485_ir_receiver_serial.setTimeout(100);
+
+	RS232_HDMI_switch_serial.begin(9600); //commenting this out works?
+	
+	pinMode(3, OUTPUT);
+	digitalWrite(3, LOW);  // Enable RS485 Receive
+	
+	debugPrint("HTControl main unit started");
+}
+
+void loop()
+{
+	RS485_ir_receiver_serial.listen();
+	if (RS485_ir_receiver_serial.available())
+	{		
+		cmd = RS485_ir_receiver_serial.readString();
+		//debugPrint("Received: " + cmd);
+		if (cmd == "HTON")
+		{
+			debugPrint("HTON Received");
+			RS232_HDMI_switch_serial.print("POWER 01\n\r");
+		}
+
+		if (cmd == "HTOFF")
+		{
+			debugPrint("HTOFF Received");
+			RS232_HDMI_switch_serial.print("POWER 00\n\r");
+		}
+		delay(10);
+	}
+}
+
+void debugPrint(String text)
+{
+	Serial.println(text);
+}
