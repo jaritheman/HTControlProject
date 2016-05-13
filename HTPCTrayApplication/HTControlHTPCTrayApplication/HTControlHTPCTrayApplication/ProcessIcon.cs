5f4cf449887dce8e05b1a8908c3edd71e7fda1bd
Jari Nevala <jari.nevala@optofidelity.com>
2016-05-13 14:06:01 +0300

    Tray application ver 1 added

---
+++ HTPCTrayApplication/HTControlHTPCTrayApplication/HTControlHTPCTrayApplication/ProcessIcon.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using HTControlHTPCTrayApplication.Properties;
+
+namespace HTControlHTPCTrayApplication
+{
+    /// <summary>
+    /// Class to capsulate NotifyIcon as per http://www.codeproject.com/Articles/290013/Formless-System-Tray-Application
+    /// </summary>
+    class ProcessIcon : IDisposable
+    {
+        /// <summary>
+        /// The NotifyIcon object.
+        /// </summary>
+        private NotifyIcon ni;                        
+        private bool cancel;
+        private bool balloonVisible;
+        private int comPortNumber; //container for comport number to be able to show it at double click
+
+        public bool Cancelled 
+        {
+            get { return this.cancel; }              
+        }
+        
+        public bool BalloonVibible
+        {
+            get { return this.balloonVisible; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProcessIcon"/> class.
+        /// </summary>
+        public ProcessIcon()
+        {
+            // Instantiate the NotifyIcon object.
+            ni = new NotifyIcon();
+        }
+
+        /// <summary>
+        /// Displays the icon in the system tray.
+        /// </summary>
+        public void Display(int comPortNumber)
+        {
+            // Put the icon in the system tray and allow it react to mouse clicks.			
+            //ni.MouseClick += new MouseEventHandler(ni_MouseClick);
+
+            ni.BalloonTipClicked += new System.EventHandler(this.ni_BalloonTipClicked);
+            ni.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.ni_MouseDoubleClick);
+            ni.BalloonTipClosed += new System.EventHandler(this.ni_BalloonTipClosed);
+
+            ni.Icon = Resources.AppIcon;                        
+            ni.Text = "HT Control HTPC Tray Application";
+            ni.Visible = true;
+
+            // Attach a context menu.
+            //ni.ContextMenuStrip = new ContextMenus().Create();
+            this.comPortNumber = comPortNumber;
+        }
+
+        private void ni_BalloonTipClosed(object sender, EventArgs e)
+        {
+            //Console.WriteLine("Balloon closed!");
+            balloonVisible = false;
+        }
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources
+        /// </summary>
+        public void Dispose()
+        {
+            // When the application closes, this will remove the icon from the system tray immediately.
+            ni.Dispose();
+        }
+
+        /// <summary>
+        /// Handles the MouseClick event of the ni control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Windows.Forms.MouseEventArgs"/> instance containing the event data.</param>
+        void ni_MouseClick(object sender, MouseEventArgs e)
+        {
+            // Handle mouse button clicks.
+            if (e.Button == MouseButtons.Left)
+            {
+                // Start Windows Explorer.
+                //Process.Start("explorer", null);
+                MessageBox.Show("left click!");
+            }
+            else if (e.Button == MouseButtons.Right)
+            {                
+                MessageBox.Show("right click!");            
+            }
+        }
+
+        private void ni_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            DialogResult res = MessageBox.Show(null, "Listening for commands on COM" + this.comPortNumber + Environment.NewLine + Environment.NewLine + "Kill application?", "HT Control HTPC Tray Application", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (res == DialogResult.Yes)
+            {                
+                Application.Exit();
+            }
+        }
+
+        private void ni_BalloonTipClicked(object sender, EventArgs e)
+        {
+            //Balloon was clicked, cancel shutdown
+            cancel = true;
+            balloonVisible = false;            
+            MessageBox.Show(null, "HTPC Shutdown Cancelled", "Cancelled", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+         
+        public void ShowBalloonTip()
+        {
+            cancel = false;
+            balloonVisible = true;
+            ni.ShowBalloonTip(10000, "HT Shutdown Initiated", "Click this shit to cancel", ToolTipIcon.Warning);
+        }
+    }
+}
---
+++ HTPCTrayApplication/HTControlHTPCTrayApplication/HTControlHTPCTrayApplication/Program.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HTControlHTPCTrayApplication
+{
+	static class Program
+	{
+		static SerialPort serialPort;
+		static int comPortNumber = 0;
+        static bool testMode = false;
+        static ProcessIcon pi = new ProcessIcon();
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			            
+            try
+            {
+                //read settings from settings.txt file
+                readSettings();
+
+                //open serial port 
+                openPort();
+
+                //show tray icon
+                pi.Display(comPortNumber);
+                
+                //make sure the application runs
+                Application.Run();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(null, ex.Message, "Application cannot run", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                closePort();
+                if (pi != null) pi.Dispose();
+            }
+		}
+
+		private static void openPort()
+		{
+            try
+            {            
+                serialPort = new SerialPort("COM" + comPortNumber, 9600, Parity.None, 8, StopBits.One);
+                serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+                serialPort.Open();
+                Console.WriteLine("Serial port in " + serialPort.PortName + " opened...");
+            }
+            catch (Exception)
+            {
+                throw new Exception("HTControlHTPCTrayApplication: Cannot open serial port in COM" + comPortNumber);                
+            }			
+		}
+
+		private static void closePort()
+		{
+            if (serialPort != null)
+            {
+                if (serialPort.IsOpen)
+                {
+                    serialPort.Close();
+                    Console.WriteLine("Serial port in " + serialPort.PortName + " closed.");
+                }
+            }
+		}
+
+		private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+		{
+			SerialPort sp = (SerialPort)sender;
+			string indata = sp.ReadLine();
+
+			Console.WriteLine("Data Received: ");
+			Console.Write(indata);
+
+			if (indata == "HTOFF\r")
+			{
+                ShutDownProcedure();                
+            }
+		}
+
+        private static void ShutDownProcedure()
+        {
+            pi.ShowBalloonTip();
+            
+            while (pi.BalloonVibible) 
+            {
+                //Wait for user to cancel while ballon is visible
+            }
+
+            if (!pi.Cancelled)
+            {
+                if (testMode)
+                {
+                    MessageBox.Show("At this point the PC would be forced to shutdown");
+                }
+                else
+                {
+                    Console.WriteLine("Shutdown imminent!");
+                    var psi = new ProcessStartInfo("shutdown", "/s /f /t 0"); //shutdown forced immediately
+                    psi.CreateNoWindow = true;
+                    psi.UseShellExecute = false;
+                    Process.Start(psi);
+                }                
+            }
+        }
+
+        private static void readSettings()
+        {
+            string settingFileName = @"settings.txt";
+            string dir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string path = Path.Combine(dir, settingFileName);
+            
+            if (File.Exists(path))
+            {
+                string[] lines = System.IO.File.ReadAllLines(path);
+
+                foreach (var line in lines)
+                {
+                    if(line.Contains('='))
+                    {
+                        string[] parts = line.Split('=');
+
+                        switch (parts[0].Trim())
+                        {
+                            case "comport":
+                                comPortNumber = int.Parse(parts[1].Trim());
+                                break;
+                            case "testmode":
+                                testMode = bool.Parse(parts[1].Trim());                                
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception ("settings.txt file not found!");
+            }            
+        }
+	}
+}
